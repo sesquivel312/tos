b5d633cdf48ce4080abf19aa4cd64cd5a336ce06
respon <e@m.c>
2020-02-02 15:57:33 -0700

    update docker build powershell script

--- scripts/tos_run.ps1
+++ scripts/tos_run.ps1
@@ -1,15 +1,24 @@
-$home_dir = $args[0]
+$home_dir = $args[0]  # get the base directory to work from
 
-write-output $home_dir
+# stop and remove the container if it exists
+docker container stop tos
+docker container rm tos
 
-set-location -path "${home_dir}\dev\docker\nginx_flask"
+set-location -path "${home_dir}\dev\docker\nginx_flask"  # cd to the docker build directory
 
-docker build --tag tos .
+# copy requirements file to build dir
+copy-item "${home_dir}\dev\tos\requirements.nix" -Destination "."
 
-set-location -path "${home_dir}\dev\tos"
+docker build --tag tos .  # build the image, no cache b/c docker is ignoring changes
 
-$docker_run = "docker run -idt --rm --name tos --hostname tos -v ${home_dir}\www:/usr/share/nginx/html -v ${home_dir}\dev\tos:/tos -p 9080:80 tos"
+set-location -path "${home_dir}\dev\tos"  # change to the flask app directory - don't recall why?
 
+# setup the docker command w/variables replaced, then execute it
+$docker_run = "docker run -idt --rm --name tos --hostname tos -v ${home_dir}\www:/usr/share/nginx/html -v ${home_dir}\dev\tos\tos:/tos -p 9080:80 tos"
 iex -command $docker_run
 
+# pip install requirements, in case they changed
+docker exec tos sh -c "python3 -m pip install -r /requirements.nix"
+
+# run the uwsgi app server w/the app
 docker exec -it tos sh -c "uwsgi --ini tos.ini --daemonize2 /var/log/uwsgi.log && /bin/bash"
